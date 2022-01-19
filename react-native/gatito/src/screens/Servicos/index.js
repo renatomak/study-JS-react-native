bfc17e1e6b7d01fd37fe9a5d3da3534ad90de9c6
Renato Marques <32239996+renatomak@users.noreply.github.com>
2022-01-19 09:03:30 -0300

    curso 2 - projeto gatito - primeiro commit

---
+++ react-native/gatito/src/screens/Servicos/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+export default function Servicos() {
+
+    return (
+        <Text>
+            Serviços
+        </Text>
+    )
+}
