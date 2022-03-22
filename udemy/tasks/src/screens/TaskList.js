1ad54ca24dd837a34601958d3f64771a453e1628
renatomak <renato.mark.silva@gmail.com>
2022-03-22 07:35:32 -0300

    feat: button add task

--- udemy/tasks/src/screens/TaskList.js
+++ udemy/tasks/src/screens/TaskList.js
@@ -17,9 +17,11 @@ import 'moment/locale/pt-br';
 import commonStyles from '../commonStyles';
 import Task from '../components/Task';
 import {data} from './data';
+import AddTask from './AddTask';
 
 export default class TaskList extends Component {
   state = {
+    showAddTask: true,
     visibleTasks: [],
     showDoneTasks: true,
     tasks: data,
@@ -60,6 +62,10 @@ export default class TaskList extends Component {
       .format('ddd, D [de] MMMM');
     return (
       <View style={styles.container}>
+        <AddTask
+          isVisible={this.state.showAddTask}
+          onCancel={() => this.setState({showAddTask: false})}
+        />
         <ImageBackground source={todayImage} style={styles.background}>
           <View style={styles.iconBar}>
             <TouchableOpacity onPress={this.toggleFilter}>
@@ -84,6 +90,12 @@ export default class TaskList extends Component {
             )}
           />
         </View>
+        <TouchableOpacity
+          activeOpacity={0.7}
+          style={styles.addButton}
+          onPress={() => this.setState({showAddTask: true})}>
+          <Icon name="plus" size={20} color={commonStyles.colors.secondary} />
+        </TouchableOpacity>
       </View>
     );
   }
@@ -122,5 +134,16 @@ const styles = StyleSheet.create({
     marginHorizontal: 20,
     justifyContent: 'flex-end',
     marginTop: Platform.OS === 'ios' ? 30 : 10,
+  },
+  addButton: {
+    position: 'absolute',
+    right: 30,
+    bottom: 30,
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    backgroundColor: commonStyles.colors.today,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
